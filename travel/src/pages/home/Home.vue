d907d1807115a58de07c070f880c17161d46c55b
mulili <muli9611@gmail.com>
2020-06-01 17:34:18 +0800

    HomeIcons component layout  & style

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from "./component/HomeHeader";
 import HomeSwiper from "./component/HomeSwiper";
+import HomeIcons from "./component/HomeIcons";
 
 export default {
   name: "Home",
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 };
 </script>
