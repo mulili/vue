d29a0f1c670fc5589090bdc2635e4b91845a55de
mulili <muli9611@gmail.com>
2020-06-02 13:22:50 +0800

    complete recommend component

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <header></header>
     <swiper></swiper>
     <icons></icons>
+    <recommend></recommend>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import Header from "./component/Header";
 import Swiper from "./component/Swiper";
 import Icons from "./component/Icons";
+import Recommend from "./component/Recommend";
 
 export default {
   name: "Home",
   components: {
     Header,
     Swiper,
-    Icons
+    Icons,
+    Recommend,
   }
 };
 </script>
