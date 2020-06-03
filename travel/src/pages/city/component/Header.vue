11912cdbcfcc175f2973251f8ba2b891e89227b6
Muli <muli9611@gmail.com>
2020-06-03 15:19:52 +0800

    complete CityHeader component and router-link (#8)
    complete CityHeader component and router-link

---
+++ travel/src/pages/city/component/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <i class="iconfont icon-back-arrow back" />
+    </router-link>
+    <span>城市选择</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityHeader"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@style/variables.styl';
+
+.header {
+  line-height: $lineHeight;
+  color: #fff;
+  text-align: center;
+  background-color: $bgColor;
+  font-size: 0.32rem;
+  position: relative;
+  left: 0;
+  top: 0;
+
+  .back {
+    font-size: 0.4rem;
+    position: absolute;
+    left: 0.16rem;
+    top: 0;
+    color: #fff;
+  }
+}
+</style>
