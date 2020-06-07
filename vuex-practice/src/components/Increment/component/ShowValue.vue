5f6351ab20044648879cc13cb35a974a01a05468
mulili <muli9611@gmail.com>
2020-06-07 13:15:48 +0800

    greeters 的使用

--- vuex-practice/src/components/Increment/component/ShowValue.vue
+++ vuex-practice/src/components/Increment/component/ShowValue.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
     <div>{{count}}</div>
+    <div>{{capitalGreeting}}</div>
     <div>{{greeting}}</div>
+    <div>{{getName(2)}}</div>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapGetters } from "vuex";
 export default {
   name: "ShowValue",
   computed: {
     // count() {
     //   return this.$store.state.count;
     // }
-    ...mapState(["count", "greeting"])
+    ...mapState(["count", "greeting"]),
+    ...mapGetters(["capitalGreeting","getName"])
   },
 
   mounted() {
