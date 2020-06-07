40595f3689df2434187235f3287388efc0936811
mulili <muli9611@gmail.com>
2020-06-07 22:24:50 +0800

    Vue.set/Vue.delete，新增或删除对象数据的属性

--- vue-api/src/App.vue
+++ vue-api/src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="app">
     <input @keyup.v="handleHitV" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <br />
+    <button @click="handleAddKey">Add key</button>
+    <button @click="handleDeleteKey">Delete key</button>
+    <ul>
+      <li v-for="key of obj" :key="key">{{key}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 Vue.config.keyCodes = { v: 86 };
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      obj: { A: "a", B: "B", C: "c" }
+    };
   },
   methods: {
-    handleHitV(e) {
+    handleHitV() {
       console.log("hit v key");
+    },
+    handleAddKey() {
+      /*  
+        data 为对象时，修改data需要注意：
+          1. 可以修改已存在的value；
+          2. 可以修改对象的引用，也就是换一个新对象；
+          3. 注意，不能利用 ./[]语法添加新属性，需要使用Vue.set(target,key/index,value)的方式；
+          4. vue也无法监测到使用 delete 删除属性后的 view 变化，需要使用Vue.delete(target,key/index)
+      */
+
+      Vue.set(this.obj, "abc", "abc");
+    },
+    handleDeleteKey() {
+      Vue.delete(this.obj, "A");
     }
   }
 };
