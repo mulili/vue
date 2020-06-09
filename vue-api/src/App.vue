9cc2e3435cc8e0e905807f0cdcdb19390c5b70b8
mulili <muli9611@gmail.com>
2020-06-09 11:00:10 +0800

    Vue生命周期之11种钩子函数

--- vue-api/src/App.vue
+++ vue-api/src/App.vue
@@ -1,21 +1,33 @@
 <template>
-  <div id="app">
-    <h5>{{firstKey}}</h5>
-    <input @keyup.v="handleHitV" />
-    <br />
-    <button @click="handleAddKey">Add key</button>
-    <button @click="handleDeleteKey">Delete key</button>
-    <ul>
-      <li v-for="(value,index) of obj" :key="index">{{value}}</li>
-    </ul>
-  </div>
+  <keep-alive>
+    <div id="app">
+      <keep-alive>
+        <first-key-value v-if="isShow" :firstKey="firstKey" ref="h5" />
+      </keep-alive>
+      <input @keyup.v="handleHitV" />
+      <br />
+      <button @click="handleAddKey">Add key</button>
+      <button @click="handleDeleteKey">Delete key</button>
+      <button @click="handleChangeFirstKey">Change firstKey</button>
+      <button @click="handleIsShow">Toggle</button>
+      <ul>
+        <li v-for="(value,index) of obj" :key="index">{{value}}</li>
+      </ul>
+    </div>
+  </keep-alive>
 </template>
 
 <script>
 import Vue from "vue";
+import FirstKeyValue from "./components/firstKeyValue";
+import logRed from "./utils/logRed";
+
 Vue.config.keyCodes = { v: 86 };
 export default {
   name: "App",
+  components: {
+    FirstKeyValue
+  },
   /* 
       1. 子组件中data 必须定义为一个函数返回对象，避免引用相同的对象，造成数据干扰；
       2. data作为返回对象的函数时，该函数接受一个参数，是当前组件的实例对象；
@@ -23,11 +35,17 @@ export default {
   data: () => {
     // console.log("vm", vm);
     return {
-      obj: { A: "a", B: "b", C: "c", D: { d: { a: "c" } } }
+      obj: { A: "a", B: "b", C: "c", D: { d: { a: "c" } } },
+      isShow: true
     };
   },
   computed: {
-    firstKey: vm => Object.keys(vm.obj)[0].toUpperCase()
+    firstKey: {
+      get: vm => Object.keys(vm.obj)[0].toUpperCase(),
+      set() {
+        this.obj = { B: "b", C: "c", D: { d: { a: "c" } } };
+      }
+    }
   },
   /* 
     watch 中的handler接收两个参数 newValue, odlValue;
@@ -41,7 +59,8 @@ export default {
         /*  
           注意：默认的呃行为不会监视嵌套的属性的变化
            deep: true，使得该回调会在任何被侦听的对象的 property 改变时被调用，不论其被嵌套多深；
-        */ 
+        */
+
         deep: true,
         handler: (value, oldValue) => {
           console.log(
@@ -80,10 +99,89 @@ export default {
     */
     handleDeleteKey() {
       Vue.delete(this.obj, "A");
+    },
+    handleChangeFirstKey() {
+      this.firstKey = `changed ${this.firstKey.toLowerCase()}`;
+    },
+    handleIsShow() {
+      this.isShow = !this.isShow;
     }
   },
+  /* 
+      所有的生命周期钩子自动绑定 this 上下文到实例中；
+      意味着不能使用 箭头函数 来定义一个生命周期函数，那样 this 会指向 undefined
+    */
+  beforeCreate() {
+    /* 
+      在实例初始化之后，
+      数据观测（ data observe ）和 event/watcher 事件配置之前被调用；
+    */
+    logRed("beforeCreate");
+    console.log(this);
+  },
+  created() {
+    /* 
+      实例创建完成后被立即调用。
+      在这一步，实例已完成以下的配置：数据观测 (data observer)，property 和方法的运算，watch/event 事件回调。
+      然而，挂载阶段还没开始，$el property 目前尚不可用。
+    */
+    logRed("created");
+    console.log(this);
+  },
+  beforeMount() {
+    /* 
+      在挂载开始之前被调用：相关的 render 函数首次被调用。
+      该钩子在服务器端渲染期间不被调用。
+     */
+    logRed("beforeMount");
+    console.log(this);
+  },
   mounted() {
-    // console.log(Vue.version);
+    /* 
+      实例被挂载后调用，
+      这时 el 已被新创建的 vm.$el 替换了，
+      如果根实例挂在到了一个文档内的元素上，当 mounted 被调用时 vm.$el 也在文档内；
+
+      注意， mounted 不会保证所有的子组件也都一起被挂载，
+      如果希望等到整个视图都渲染完毕，可以在 mounted 内部使用 vm.$nextTick
+
+      该钩子在服务器端渲染期间不被调用。
+    */
+    logRed("mounted");
+    console.log(this);
+  },
+  beforeUpdate() {
+    /*
+      数据更新时调用，发生在虚拟 DOM 打补丁（ patch ）之前，
+      这里适合在更新之前访问现有的 DOM ，比如手动移除已添加的事件监听器
+      
+      该钩子在服务器端渲染期间不被调用，因为只有初次渲染会在服务端进行。
+    */
+    logRed("beforeUpdate");
+    console.table(this.$refs.h5.$refs.firstKey.innerText);
+    console.log(this);
+  },
+  updated() {
+    /* 
+      由于数据更改导致的虚拟 DOM 重新渲染和打补丁，在这之后会调用该钩子；
+      当这个钩子被调用时，组件 DOM 已经更新，所以现在可以执行依赖于 DOM 的操作。
+
+      注意 updated 不会保证所有的紫钻也都一起被重绘。
+
+      如果想得到整个视图都重回完毕，可以在 updated 里使用 vm.$nextTick;
+
+      该钩子在服务器端渲染期间不被调用。
+    */
+    logRed("updated");
+    console.log(this);
+    console.table(this.$refs.h5.$refs.firstKey.innerText);
+  },
+  errorCaptured(err, component, info) {
+    logRed("errorCapture");
+    console.log("err", err);
+    console.log("component", component);
+    console.log("info", info);
+    return false;
   }
 };
 </script>
---
+++ vue-api/src/components/firstKeyValue.vue
@@ -0,0 +1,49 @@
+<template>
+  <h5 ref="firstKey">{{firstKey}}</h5>
+</template>
+
+<script>
+import logRed from "../utils/logRed";
+export default {
+  name: "FirstKeyValue",
+  props: {
+    firstKey: {
+      type: String,
+      required: true
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      throw new TypeError("child component error");
+    }, 2000);
+  },
+  activated() {
+    // 该钩子函数在 keep-alive 缓存的组件激活时调用
+    logRed("activated");
+    console.log(this);
+  },
+  deactivated() {
+    // 该钩子函数在 keep-alive 缓存的组件停用时调用
+    logRed("deactivated");
+    console.log(this);
+  },
+  beforeDestroy() {
+    // 实例被销毁之前调用，在这一步，实例仍然完全可用
+    logRed("beforeDestroy");
+  },
+  destroyed() {
+    /* 
+      实例销毁后调用，
+      该钩子被调用后，
+        对应Vue实例的所有指令都被解绑；
+        所有的的事件监听器被移除；
+        所有的子实例也都被销毁；
+      不能在该钩子函数中对组件实例做任何有效的操作；
+    */
+    logRed("destroyed");
+  }
+};
+</script>
+
+<style scoped>
+</style>
