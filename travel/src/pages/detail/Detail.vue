6b178170dd4f42db4920113953953f6936366658
mulili <muli9611@gmail.com>
2020-06-30 23:57:21 +0800

    使用mock数据模拟API请求，替换硬编码的数据

--- travel/src/pages/detail/Detail.vue
+++ travel/src/pages/detail/Detail.vue
@@ -1,61 +1,66 @@
 <template>
-  <div class="wrapper">
-    <detail-header />
-    <detail-banner />
-    <detail-list :list="list" />
-    <div class="container"></div>
-  </div>
+	<div class="wrapper">
+		<detail-header />
+		<detail-banner :sightName="sightName" :bannerImg="bannerImg" :galleryImgS="galleryImgS" />
+		<detail-list :list="categoryList" />
+		<div class="container"></div>
+	</div>
 </template>
 
 <script>
+import { selfAxios } from "@utils";
+
 import DetailBanner from "./component/Banner";
 import DetailHeader from "./component/Header";
 import DetailList from "./component/List";
 
 export default {
-  name: "Detail",
-  props: ["id"],
-  components: {
-    DetailBanner,
-    DetailHeader,
-    DetailList
-  },
-  data() {
-    return {
-      list: [
-        {
-          title: "成人票",
-          children: [
-            {
-              title: "成人三馆联票",
-              children: [{ title: "七折优惠" }]
-            },
-            {
-              title: "成人五馆联票",
-              children: [{ title: "五折优惠" }]
-            }
-          ]
-        },
-        {
-          title: "学生票",
-          children: [{ title: "学生三馆联票" }, { title: "学生五馆联票" }]
-        },
-        {
-          title: "儿童票",
-          children: [{ title: "儿童三馆联票" }, { title: "儿童五馆联票" }]
-        },
-        {
-          title: "特惠票",
-          children: [{ title: "特惠三馆联票" }, { title: "特惠五馆联票" }]
-        }
-      ]
-    };
-  }
+	name: "Detail",
+	props: ["id"],
+	components: {
+		DetailBanner,
+		DetailHeader,
+		DetailList
+	},
+	data() {
+		return {
+			sightName: "",
+			bannerImg: "",
+			galleryImgS: [],
+			categoryList: []
+		};
+	},
+	methods: {
+		getDetailSuccess(res) {
+			if (res.data) {
+				this.sightName = res.data.sightName;
+				this.bannerImg = res.data.bannerImg;
+				this.galleryImgS = res.data.galleryImgS;
+				this.categoryList = res.data.categoryList;
+			}
+		},
+		getDetailError(err) {
+			console.error(err);
+		},
+		getDetailInfo() {
+			const id = this.params;
+			return selfAxios.get("/api/detail.json", {
+				params: {
+					id
+				}
+			});
+		}
+	},
+	mounted() {
+		this.getDetailInfo()
+			.then(this.getDetailSuccess)
+			.catch(this.getDetailError);
+	}
 };
 </script>
 
 <style lang="stylus" scoped>
 .container {
-  height: 20rem;
+	height: 20rem;
 }
 </style>
--- travel/src/pages/detail/component/Banner.vue
+++ travel/src/pages/detail/component/Banner.vue
@@ -1,110 +1,103 @@
 <template>
-  <div>
-    <div class="banner" @click="toggleShowGallery">
-      <div class="img-wrapper">
-        <img
-          class="banner-img"
-          src="https://imgs.qunarzz.com/p/tts2/1807/43/0cdbb72913b35a02.jpg_r_390x260x95_2c5e2446.jpg"
-          alt="banner image"
-        />
-      </div>
-      <div class="banner-info">
-        <p class="banner-name">大连圣亚海洋世界（AAAA景区）</p>
-        <p class="banner-number">
-          <i class="iconfont icon-img banner-icon" />
-          39
-        </p>
-      </div>
-    </div>
-    <common-gallery @closeGallery="toggleShowGallery" v-if="isShowGallery" :imgUrls="imgUrls" />
-  </div>
+	<div>
+		<div class="banner" @click="toggleShowGallery">
+			<div class="img-wrapper">
+				<img class="banner-img" :src="bannerImg" alt="banner image" />
+			</div>
+			<div class="banner-info">
+				<p class="banner-name">{{sightName}}</p>
+				<p class="banner-number">
+					<i class="iconfont icon-img banner-icon" />
+					39
+				</p>
+			</div>
+		</div>
+		<common-gallery @closeGallery="toggleShowGallery" v-if="isShowGallery" :imgUrls="galleryImgS" />
+	</div>
 </template>
 
 <script>
 import CommonGallery from "@common/gallery/Gallery";
 
 export default {
-  name: "DetailBanner",
-  components: {
-    CommonGallery
-  },
-  data() {
-    return {
-      isShowGallery: false,
-      imgUrls: [
-        {
-          id: 0,
-          url:
-            "https://imgs.qunarzz.com/p/tts8/1807/6f/1c13592a0ae23202.jpg_r_390x260x95_f4f34290.jpg"
-        },
-        {
-          id: 1,
-          url:
-            "https://imgs.qunarzz.com/p/tts4/1807/84/3e9d39f18e97ff02.jpg_r_390x260x95_0a3c6067.jpg"
-        },
-        {
-          id: 2,
-          url:
-            "https://imgs.qunarzz.com/p/tts9/1807/93/1578f06b9e0e5702.jpg_r_390x260x95_24d44773.jpg"
-        }
-      ]
-    };
-  },
-  methods: {
-    toggleShowGallery() {
-      this.isShowGallery = !this.isShowGallery;
-    }
-  }
+	name: "DetailBanner",
+	components: {
+		CommonGallery
+	},
+	props: {
+		sightName: {
+			type: String,
+			required: true
+		},
+		bannerImg: {
+			type: String,
+			required: true
+		},
+		galleryImgS: {
+			type: Array,
+			required: true
+		}
+	},
+	data() {
+		return {
+			isShowGallery: false
+		};
+	},
+	methods: {
+		toggleShowGallery() {
+			this.isShowGallery = !this.isShowGallery;
+		}
+	}
 };
 </script>
 
 <style lang="stylus" scoped>
 .banner {
-  position: relative;
-  top: 0;
-  left: 0;
+	position: relative;
+	top: 0;
+	left: 0;
 
-  .img-wrapper {
-    overflow: hidden;
-    height: 0;
-    padding-bottom: 55%;
+	.img-wrapper {
+		overflow: hidden;
+		height: 0;
+		padding-bottom: 55%;
 
-    .banner-img {
-      width: 100%;
-    }
-  }
+		.banner-img {
+			width: 100%;
+		}
+	}
 
-  .banner-info {
-    display: flex;
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    line-height: 0.6rem;
-    color: #fff;
-    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+	.banner-info {
+		display: flex;
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		line-height: 0.6rem;
+		color: #fff;
+		background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
 
-    .banner-name {
-      flex: 1;
-      padding: 0 0.2rem;
-      font-size: 0.32rem;
-    }
+		.banner-name {
+			flex: 1;
+			padding: 0 0.2rem;
+			font-size: 0.32rem;
+		}
 
-    .banner-number {
-      height: 0.32rem;
-      line-height: 0.32rem;
-      padding: 0 0.4rem;
-      border-radius: 0.2rem;
-      background-color: black;
-      opacity: 0.8;
-      font-size: 0.24rem;
-      margin: 0.14rem 0.2rem 0 0;
+		.banner-number {
+			height: 0.32rem;
+			line-height: 0.32rem;
+			padding: 0 0.4rem;
+			border-radius: 0.2rem;
+			background-color: black;
+			opacity: 0.8;
+			font-size: 0.24rem;
+			margin: 0.14rem 0.2rem 0 0;
 
-      .banner-icon {
-        font-size: 0.24rem;
-        padding-left: 0.1rem;
-      }
-    }
-  }
+			.banner-icon {
+				font-size: 0.24rem;
+				padding-left: 0.1rem;
+			}
+		}
+	}
 }
 </style>
