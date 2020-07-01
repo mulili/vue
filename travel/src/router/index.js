2593ef8c3112aca8a9a627269ab8f9e049267699
mulili <muli9611@gmail.com>
2020-07-01 09:07:24 +0800

    fix，调整react-router的滚动行为

--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -12,5 +12,17 @@ const routes = [
 ];
 
 export default new Router({
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    /*
+      第三个参数 savedPosition 当且仅当 popstate 导航 (通过浏览器的 前进/后退 按钮触发) 时才可用；
+      返回 savedPosition，在按下 后退/前进 按钮时，就会像浏览器的原生表现那样（记住页面位置）；
+
+      return {x:0,y:0},对于所有路由导航，简单地让页面滚动到顶部；
+    */
+    return {
+      x: 0,
+      y: 0
+    };
+  }
 });
