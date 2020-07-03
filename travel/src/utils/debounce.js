b9094d75874b590e21605d0a308ce58a6f0ca22e
huhan-y <huhan_y@163.com>
2020-07-03 14:24:05 +0800

    优化debounce (#30)
    * 优化debounce ，修复字母不复原的bug
    
    Co-authored-by: 胡晗 <huhan@hongyaodz.cn>

--- travel/src/utils/debounce.js
+++ travel/src/utils/debounce.js
@@ -1,8 +1,24 @@
-let timer = null;
+/**
+ * 返回一个防抖化后的函数 debounceFn ,
+ * 重复调用 debounceFn 时会采用防抖策略，而且会返回一个清除计时器的方法 clearTimer
+ * 调用 clearTimer 时会清除防抖时最后一个计时器
+ */
 const debounce = (fn, time=16) => {
-  if (timer) {
-    clearTimeout(timer);
+  let timer
+  let start = Date.now()
+  const clearTimer = () => clearTimeout(timer)
+  return function debounceFn(...args) {
+    const current = Date.now()
+    clearTimer()
+    if(current - start > time){
+      fn.apply(this,args)
+      start = current
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this,args)
+      }, time)
+    }
+    return clearTimer
   }
-  timer = setTimeout(fn, time);
 };
 export default debounce;
