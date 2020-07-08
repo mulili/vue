09e132f1b3df1f648cc8b3d45d12d232698748d6
mulili <muli9611@gmail.com>
2020-07-08 12:51:45 +0800

    fix(build): 为不同环境设置不同的api域名

--- travel/src/utils/selfAxios.js
+++ travel/src/utils/selfAxios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const selfAxios = axios.create();
+const selfAxios = axios.create({
+  // api base url
+  baseURL: process.env.BASE_URL
+});
 selfAxios.interceptors.response.use(
   response => response.data,
   error => Promise.reject(error)
