bd304397012802dd83f2bd044100bd20129c0d75
mulili <muli9611@gmail.com>
2020-07-08 16:21:33 +0800

    feat(prod): 设置 prod 的 baseURL

--- travel/config/prod.env.js
+++ travel/config/prod.env.js
@@ -1,5 +1,5 @@
 "use strict";
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"/travel/"'
+  BASE_URL: '"https://mulili.github.io/travel"'
 };
--- travel/src/utils/selfAxios.js
+++ travel/src/utils/selfAxios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const commonURL = "/static/mock";
 const selfAxios = axios.create({
   // api base url
-  baseURL: process.env.BASE_URL
+  baseURL: `${process.env.BASE_URL}${commonURL}`
 });
 selfAxios.interceptors.response.use(
   response => response.data,
