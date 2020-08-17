b8c5fbe429c815e9a952e572ee94dab3b9227ed8
mulili <muli9611@gmail.com>
2020-08-17 21:17:37 +0800

    feature(travel): 剥离去哪网项目

--- travel/src/store/state.js
+++
@@ -1,8 +0,0 @@
-import { selfLocalStorage } from "@utils";
-
-const { getItem } = selfLocalStorage;
-
-const state = {
-  city: getItem("city") || "北京"
-};
-export default state;
--- travel/src/utils/index.js
+++
@@ -1,4 +0,0 @@
-export { default as selfAxios } from "./selfAxios";
-export { debounce } from "./debounce";
-export { default as scrollTop } from "./scrollTop";
-export { default as selfLocalStorage } from "./selfLocalStorage";
